2ca152877397784e38a46e70819cee5c464977b0
sajanaka00 <nivin.2019452@iit.ac.lk>
2020-06-05 19:48:11 +0530

    second commit

--- app/src/main/java/com/example/eclothing/MainActivity.java
+++ app/src/main/java/com/example/eclothing/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.eclothing;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.BreakIterator;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    EditText username, password;
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = findViewById(R.id.username);
+        password = findViewById(R.id.password);
+        button = findViewById(R.id.button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String accUsername = username.getText().toString();
+                String accPassword = password.getText().toString();
+
+                // Validating the login form
+                if(username.getText().toString().equals("") || password.getText().toString().equals("")) {
+                    // If name or password is not entered
+                    System.out.println("Both Username and Password are required!");
+                }
+                else {
+                    System.out.println("Username: " +accUsername);
+                    System.out.println("Password: " +accPassword);
+                }
+            }
+        });
     }
+
 }
